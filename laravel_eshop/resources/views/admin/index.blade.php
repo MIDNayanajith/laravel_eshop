bb0288b58f7d0f605a993bc66f07d9b282bbd402
MIDNayanajith <isuranga_2020736@fot.ruh.ac.lk>
2025-05-12 13:09:39 +0530

    ddevelop login and register and middleware

---
+++ laravel_eshop/resources/views/admin/index.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.app')
+
+@section('content')
+    <h1>ADMIN DASHBOARD</h1>
+@endsection
--- laravel_eshop/routes/web.php
+++ laravel_eshop/routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\AdminController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\UserController;
+use App\Http\Middleware\AuthAdmin;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
@@ -9,3 +12,14 @@ use Illuminate\Support\Facades\Route;
 Auth::routes();
 
 Route::get('/', [HomeController::class, 'index'])->name('home.index');
+
+//Group for user routes
+Route::middleware(['auth'])->group(function(){
+    Route::get('/account-dashboard',[UserController::class,'index'])->name('user.index');
+});
+
+//Group for admin routes
+
+Route::middleware(['auth',AuthAdmin::class])->group(function(){
+    Route::get('/admin',[AdminController::class,'index'])->name('admin.index');
+});
