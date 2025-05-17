5bf4d71a58dd7581b7ca457d4a6866351693aaeb
MIDNayanajith <isuranga_2020736@fot.ruh.ac.lk>
2025-05-17 20:20:20 +0530

    develop admin add product function

--- laravel_eshop/app/Http/Controllers/AdminController.php
+++ laravel_eshop/app/Http/Controllers/AdminController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Brand;
 use App\Models\Category;
+use App\Models\Product;
 use Illuminate\Http\Request;
 use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\File;
@@ -198,6 +199,106 @@ class AdminController extends Controller
         }
         $category->delete();
         return redirect()->route('admin.categories')->with('status','Category has been deleted successfully!');
+    }
+
+    // controller functions for products
+
+    public function products()
+    {
+        $products = Product::orderBy('created_at','DESC')->paginate(10);
+        return view('admin.products', compact('products'));
+    }
+
+    public function product_add()
+    {
+        $categories = Category::select('id','name')->orderBy('name')->get();
+        $brands = Brand::select('id','name')->orderBy('name')->get();
+        return view('admin.products-add', compact('categories','brands'));
+    }
+
+    public function product_store(Request $request)
+    {
+        $request->validate([
+        'name' => 'required|string|max:255',
+        'slug' => 'required|unique:products,slug',
+        'short_description' => 'required',
+        'description' => 'required',
+        'regular_price' => 'required|numeric|min:0',
+        'sale_price' => 'nullable|numeric|min:0',
+        'SKU' => 'required',
+        'stock_status' => 'required|in:instock,outstock',
+        'featured' => 'required',
+        'quantity' => 'required|integer|min:0',
+        'image' => 'required|mimes:png,jpg,jpeg|max:2048', // or use 'image' => 'nullable|image' if uploading file
+        'category_id' => 'required',
+        'brand_id' => 'required',
+    ]);
+
+        $product = new Product();
+        $product->name = $request->name;
+        $product->slug = Str::slug($request->name);
+        $product->short_description = $request->short_description;
+        $product->description = $request->description;
+        $product->regular_price = $request->regular_price;
+        $product->sale_price = $request->sale_price;
+        $product->SKU = $request->SKU;
+        $product->stock_status = $request->stock_status;
+        $product->featured = $request->featured;
+        $product->quantity = $request->quantity;
+        $product->category_id = $request->category_id;
+        $product->brand_id = $request->brand_id;
+
+        $current_timestamp = Carbon::now()->timestamp;
+
+        if($request->hasFile('image'))
+        {
+            $image = $request->file('image');
+            $imageName = $current_timestamp .'.' .$image->extension();
+            $this->GenerateProductThumbnailImage($image,$imageName);
+            $product->image = $imageName;
+        }
+
+        $gallery_arr = array();
+        $gallery_images = "" ;
+        $counter = 1;
+
+        if($request->hasFile('images'))
+        {
+            $allowedfileExtion = ['jpg','png','jpeg'];
+            $files = $request->file('images');
+            foreach($files as $file)
+            {
+                $gextension = $file->getClientOriginalExtension();
+                $gcheck = in_array($gextension, $allowedfileExtion) ;
+                if($gcheck)
+                {
+                    $gfileName = $current_timestamp . '.' .$counter .'.' .$gextension;
+                    $this->GenerateProductThumbnailImage($file,$gfileName);
+                    array_push($gallery_arr, $gfileName);
+                    $counter = $counter +1;
+                }
+            }
+            $gallery_images = implode(',',$gallery_arr);
+        }
+        $product->images = $gallery_images;
+        $product->save();
+        return redirect()->route('admin.products')->with('status','Product has been added Successfully !');
+
+    }
+
+    public function GenerateProductThumbnailImage($image,$imageName)
+    {
+        $destinationPathThumbnail = public_path('uploads/products/thumbnails');
+        $destinationPath = public_path('uploads/products');
+        $img = Image::read($image->path());
+        $img->cover(540,689,"top");
+        $img->resize(540,689,function($constraint){
+            $constraint->aspectRatio();
+        })->save($destinationPath.'/'.$imageName);
+
+         $img->resize(104,104,function($constraint){
+            $constraint->aspectRatio();
+        })->save($destinationPathThumbnail.'/'.$imageName);
     }
 }
 
