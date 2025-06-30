9dbc4291f91c9f7cef3d06e04bed81ada1fb86f8
MIDNayanajith <isuranga_2020736@fot.ruh.ac.lk>
2025-06-30 10:19:29 +0530

    product

--- laravel_eshop/routes/web.php
+++ laravel_eshop/routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\CartController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ShopController;
 use App\Http\Controllers\UserController;
 use App\Http\Middleware\AuthAdmin;
 use Illuminate\Support\Facades\Auth;
@@ -12,7 +14,11 @@ use Illuminate\Support\Facades\Route;
 Auth::routes();
 
 Route::get('/', [HomeController::class, 'index'])->name('home.index');
+Route::get('/shop',[ShopController::class,'index'])->name('shop.index');
+Route::get('/shop/{product_slug}',[ShopController::class,'product_details'])->name('shop.product.details');
 
+Route::get('/cart',[CartController::class,'index'])->name('cart.index');
+Route::post('/cart/add',[CartController::class,'add_to_cart'])->name('cart.add');
 //Group for user routes
 Route::middleware(['auth'])->group(function(){
     Route::get('/account-dashboard',[UserController::class,'index'])->name('user.index');
@@ -24,10 +30,10 @@ Route::middleware(['auth',AuthAdmin::class])->group(function(){
     Route::get('/admin',[AdminController::class,'index'])->name('admin.index');
     Route::get('/admin/brands',[AdminController::class,'brands'])->name('admin.brands');
     Route::get('/admin/brand/add',[AdminController::class,'add_brand'])->name('admin.brand.add');
-    Route::post('admin/brand/store',[AdminController::class,'brand_store'])->name('admin.brand.store');
+    Route::post('/admin/brand/store',[AdminController::class,'brand_store'])->name('admin.brand.store');
     Route::get('/admin/brand/edit/{id}',[AdminController::class,'brand_edit'])->name('admin.brand.edit');
-    Route::put('admin/brand/update',[AdminController::class,'brand_update'])->name('admin.brand.update');
-    Route::delete('admin/brand/{id}/delete',[AdminController::class,'brand_delete'])->name('admin.brand.delete');
+    Route::put('/admin/brand/update',[AdminController::class,'brand_update'])->name('admin.brand.update');
+    Route::delete('/admin/brand/{id}/delete',[AdminController::class,'brand_delete'])->name('admin.brand.delete');
 
     // category routes
 
@@ -36,11 +42,14 @@ Route::middleware(['auth',AuthAdmin::class])->group(function(){
     Route::post('/admin/category/store',[AdminController::class,'category_store'])->name('admin.category.store');
     Route::get('/admin/category/edit/{id}',[AdminController::class,'catergory_edit'])->name('admin.category.edit');
     Route::put('/admin/category/update',[AdminController::class,'category_update'])->name('admin.category.update');
-    Route::delete('admin/category/{id}/delete',[AdminController::class,'category_delete'])->name('admin.category.delete');
+    Route::delete('/admin/category/{id}/delete',[AdminController::class,'category_delete'])->name('admin.category.delete');
 
     // products routes
 
     Route::get('/admin/products',[AdminController::class,'products'])->name('admin.products');
     Route::get('/admin/products/add',[AdminController::class,'product_add'])->name('admin.product.add');
-    Route::post('admin/products/store',[AdminController::class,'product_store'])->name('admin.product.store');
+    Route::post('/admin/products/store',[AdminController::class,'product_store'])->name('admin.product.store');
+    Route::get('/admin/products/edit/{id}',[AdminController::class,'product_edit'])->name('admin.product.edit');
+    Route::put('/admin/products/update',[AdminController::class,'product_update'])->name('admin.product.update');
+    Route::delete('/admin/products/{id}/delete',[AdminController::class,'product_delete'])->name('admin.product.delete');
 });
